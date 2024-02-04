eecb123730cfe7bc844f5f017d30b1368e1f1b50
Ian Carver <iancarver@ians-air-2.home>
2024-02-04 14:19:36 -0400

    lab2

---
+++ labs/lab2/L2_Carver_Ian_README.md
@@ -0,0 +1,14 @@
+Lab 2
+
+Ian Carver
+B00888694
+in423438@dal.ca
+
+Resoruces for answering questions about scopes and when let and const was added to js:
+https://dal.brightspace.com/d2l/le/content/308886/viewContent/4041926/View
+
+filezilla link: 
+https://web.cs.dal.ca/~carver/csci3172/lab2/
+
+gitlab link:
+https://git.cs.dal.ca/carver/csci3172.git
--- labs/lab2/index.js
+++ labs/lab2/index.js
@@ -1,6 +1,8 @@
 const radius = 6;
+const length = 20;
+const width = 15;
 
-function calcArea(num){
+function calcAreaCircle(num){
     //get pi from library
     const pi = Math.PI;
 
@@ -13,21 +15,46 @@ function calcArea(num){
 
 function arithmeticFuncs(num1, num2, s){
     
-    
-    if(s == 1){
-        const result = num1 + num2;
+    if (s=='add'){
+        let addition = num1 + num2;
+        console.log(addition);
+        return(addition);
+    } 
+    else if (s=='subtract'){
+        let subtract = num1 - num2;
+        console.log(subtract);
+        return(subtract);
+    } 
+    else if (s=='multiply') {
+        let multiply = num1 * num2;
+        console.log(multiply);
+        return(multiply);
+    } 
+    else if (s=='divide'){
+        let divide = num1 / num2;
+        console.log(divide);
+    }else{
+        let invalid = "not valid input";
+        console.log(invalid);
     }
-    else if(s == 2){
-        const result = num1 - num2;
-    }
-    else if(s == 3){
-        const result = num1 * num2;
-    }
-    else if(s == 4){
-        const result = num1 / num2;
-    }
-    console.log(result);
 }
 
-arithmeticFuncs(10,2,1);
-calcArea(radius);
+    function calcAreaRec(l,w){
+        let result = l * w;
+        console.log(result);
+    }
+
+
+calcAreaCircle(radius);
+arithmeticFuncs(10,2,"divide");
+calcAreaRec(length,width);
+
+
+
+// Questions
+
+// a) The scope of a Const variable depends on whether the varible is global or local, if the const varible is global
+// that means the varible can be accessed anywhere in the script. If the scope of the const variable is local such
+// as it has been initalized in a function that it can only be called/ accessed in that function. 
+
+// b) The let and const keywords were added to javascript in 2015 

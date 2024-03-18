e67c49afa91c7a28fd46b313cc0c55571e166aba
Ian Carver <iancarver@Ians-MacBook-Air-2.local>
2024-03-18 17:58:18 -0300

    lab6

---
+++ labs/lab6/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+        <p>
+          This is Ian's React App
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div id='lab'>
+      <h1>Lab 6</h1>
+            <h2>B00888694</h2>
+        </div>
+    </div>
+  );
+}
+
+export default App;
